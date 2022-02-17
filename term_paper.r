4e7ff0dbeaf69508e08e970ce06171c37bb1e9e4
isabella hochschild <ihochschild@gmail.com>
2022-02-17 18:17:10 -0500

    update term_paper.r

--- term_paper.r
+++ term_paper.r
@@ -2,7 +2,7 @@
 
 set.seed(012345678) # set seed
 N <- 1000 # number of elections; may vary geographically
-beta <-0.4 # sensitivity of preferences to changes in public opinion regarding the Big Lie
+beta <- 0.65 # sensitivity of preferences to changes in candidates' stances regarding the Big Lie
 
 standardize <- function(x){(x-min(x))/(max(x)-min(x))} # helper function to constrain values between 0 and 1
 
@@ -10,24 +10,30 @@ v_norm <- 0 + rnorm(N) # assumes each candidate's platform has about the same qu
 
 v <- standardize(v_norm) # makes all values of v be between 0 (denying Big Lie) and 1 (affirming Big Lie)
 
+# create histogram of median voter stance data
 hist(v,
      main="Distribution of Median GOP Voter Stances on the Big Lie", 
      xlab="Median Voter Stance", 
      border="#7ab2bf", 
      col="#baebf7")
 
-# We equate "marginal costs" with the existing political heavy-lifting of devising concrete stances on issues ranging from abortion rights to Build Back Better; we will assume for this model's sake that the two candidates have parallel "cost structures" around 0 because they both have the same public beliefs and campaign promises, but perhaps with slight differences accounted for by drawing from a normal distribution.
-c_A <- 0 + rnorm(N)
-c_B <- 0 + rnorm(N)
+# We equate "marginal costs" with the existing political heavy-lifting of devising concrete stances on issues ranging from abortion rights to Build Back Better; we will assume for this model's sake that the two candidates have parallel "cost structures" around 0 because they share the same public beliefs and campaign promises, but with perhaps slight differences accounted for by drawing from a normal distribution.
+c_A <- standardize(rnorm(N))
+c_B <- standardize(rnorm(N))
 
-# Nash equilibrium
-# calculates where public actions relating to Big Lie (whether it comes from answering questions in interviews, releasing documents, etc.) lies on a spectrum from -1 (Denying Big Lie) to 1 (Affirming Big Lie)
-p_A <- function(v, c_A, c_B) {(v + 3 + 2*beta*c_A + beta*c_B)/(3*beta)}
-p_B <- function(v, c_A, c_B) {(-v + 3 + 2*beta*c_B + beta*c_A)/(3*beta)}
+# Calculate equilibrium
+# Finds where public actions relating to Big Lie (whether it comes from answering questions in interviews, releasing documents, etc.) lies on a spectrum from 0 (Denying Big Lie) to 1 (Affirming Big Lie)
+p_A <- function(v, c_A, c_B) {
+        return (standardize((v + 3 + 2*beta*c_A + beta*c_B)/(3*beta)))
+        }
+p_B <- function(v, c_A, c_B) {
+        return (standardize((-v + 3 + 2*beta*c_B + beta*c_A)/(3*beta)))
+        }
 
-data <- cbind(p_A(v, c_A, c_B), p_B(v, c_A, c_B),)
-colnames(data) <- c("Candidate A", "Candidate B")
+data <- cbind(p_A(v, c_A, c_B), p_B(v, c_A, c_B), v)
+colnames(data) <- c("Candidate A", "Candidate B", "Median Voter")
 
+# Generate Data Summary & print first few lines of data matrix to embed in oaoer
 print(head(data,10))
 summary(data)
 
@@ -36,13 +42,9 @@ x_A <- standardize((v - beta*p_A(v, c_A, c_B) + beta*p_B(v, c_A, c_B))/2)
 
 plot(x_A, 
      (p_A(v, c_A, c_B) - p_B(v, c_A, c_B)),
-     main="Differences of Outward Political Stances Regarding Big Lie",
+     main="Differences of Outward Political Stances\nRegarding Big Lie vs. Point of Voter Indifference",
      xlab="Point of Voter Indifference",
      ylab="Difference between Public Affirmation of Big Lie",
 )
 
-# as you can see, the differences tend to 0, with a few anomalies
-
-# plots voter demand against relative social cost of voting for a particular candidate
-
-# look into Hotelling's original paper discussing this very topic
+# As you can see, the differences between Candidate A and Candidate B's stances tend to 0, with a few anomalies
